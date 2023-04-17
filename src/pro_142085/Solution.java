e153ce4835b9f077538cc5e3b0f088cca227ad62
gosekose <ksyn1611@naver.com>
2023-04-17 15:41:45 +0900

    프로그래머스 디펜스 queue

---
+++ src/pro_142085/Solution.java
@@ -0,0 +1,22 @@
+package pro_142085;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        
+        for (int round = 0; round < enemy.length; round++) {
+            queue.add(enemy[round]);
+            
+            if (queue.size() > k)
+                n -= queue.poll();
+            
+            if (n < 0)
+                return round;
+        }
+        
+        return enemy.length;
+    }
+}
