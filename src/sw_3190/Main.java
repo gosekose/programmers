31886cfc0c9a22920785906745873f5ccdaaa4b7
gosekose <ksyn1611@naver.com>
2023-05-05 02:45:47 +0900

    방향 전환 row, col 주석 추가

--- src/sw_3190/Main.java
+++ src/sw_3190/Main.java
@@ -12,18 +12,18 @@ public class Main {
         int N = parseInt(br.readLine());
         int K = parseInt(br.readLine());
 
-        Game game = new Game(N);
+        Game game = new Game(N); // game 인스턴스 선언
 
         StringTokenizer st;
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            game.setApple(parseInt(st.nextToken()), parseInt(st.nextToken()));
+            game.setApple(parseInt(st.nextToken()), parseInt(st.nextToken())); // 사과 등록
         }
 
         int L = parseInt(br.readLine());
         for (int i = 0; i < L; i++) {
             st = new StringTokenizer(br.readLine());
-            game.addDirection(parseInt(st.nextToken()), st.nextToken());
+            game.addDirection(parseInt(st.nextToken()), st.nextToken()); // 시간에 맞춰 변경해야하는 시간과 방향 설정
         }
 
         System.out.println(game.run());
@@ -33,39 +33,39 @@ public class Main {
 class Game {
 
     int n;
-    int time;
-    boolean[][] nowVisited;
-    boolean[][] map;
-    char nowDir = 'R';
-    List<Tail> tails = new LinkedList<>(); // 꼬리 추가 및 삭제
-    Queue<Direction> dirs = new ArrayDeque<>();
+    int time; // 출력해야 하는 시간
+    boolean[][] nowVisited; // 현재 뱀의 몸(머리, 꼬리 포함)이 걸쳐 있는지 판단
+    boolean[][] map; // 사과 있는지 판단
+    char nowDir = 'R'; // 현재 방향 (계속 바뀜)
+    List<Tail> tails = new LinkedList<>(); // 꼬리 추가 및 삭제를 위한 링크드 리스트
+    Queue<Direction> dirs = new ArrayDeque<>(); // 시간에 맞춰 방향 변경을 위한 큐
 
     Game (int n) {
         this.n = n;
-        map = new boolean[n + 1][n + 1];
+        map = new boolean[n + 1][n + 1]; // n by n 이므로 n + 1로 초기화
         nowVisited = new boolean[n + 1][n + 1];
     }
 
     void setApple(int row, int col) {
-        map[row][col] = true; // apple
+        map[row][col] = true; // apple 설정
     }
 
     void addDirection(int ttl, String dir) {
-        dirs.add(new Direction(ttl, dir));
+        dirs.add(new Direction(ttl, dir)); // 시간에 맞는 방향 설정
     }
 
     int run() {
 
         int r = 1; // 행
         int c = 1; // 열
-        tails.add(new Tail(r, c));
+        tails.add(new Tail(r, c)); // 시작은 머리와 꼬리가 동일 하므로 꼬리에 추가
 
         while (true) { // 반복
 
             if (!dirs.isEmpty() && time == dirs.peek().ttl) { //비어 있지 않고 ttl와 같음
-                Direction nextDir = dirs.poll();
+                Direction nextDir = dirs.poll(); // 다음 방향을 위한 Direction 인스턴스 큐에서 빼기
 
-                if (nowDir == 'U') { // 위
+                if (nowDir == 'U') { // 각 방향에 맞춰 회전 후 방향 재조정
                     if (nextDir.dir.equals("L")) nowDir = 'L';
                     else nowDir = 'R';
                 }
@@ -86,7 +86,7 @@ class Game {
                 }
             }
 
-            // move
+            // 회전을 마친 경우 혹은 회전이 없는 경우 nowDir에 방향에 맞춰 움직이기 시작
             switch (nowDir) {
                 case 'U':
                     r--;
@@ -102,8 +102,8 @@ class Game {
                     break;
             }
 
-            if (!isValid(r, c)) {
-                time++;
+            if (!isValid(r, c)) { // 이동하려는 곳이 인덱스에 벗어나거나, 몸통이 있는 곳이라면 종료
+                time++; // 종료시간은 마지막 움직이는 경우도 포함이므로
                 break;
             }
 
@@ -123,7 +123,7 @@ class Game {
                 map[r][c] = false; // 사과 제거하기
             }
 
-            time++;
+            time++; // 모든 움직임이 끝났으므로 시간++
         }
 
         return time;
@@ -134,13 +134,9 @@ class Game {
         return r > 0 && r <= n && c > 0 && c <= n && !nowVisited[r][c];
     }
 
-    boolean isNotAppleAndAtTail(int r, int c, Tail tail) {
-        return !map[r][c] && tail.r == r && tail.c == c; // 사과가 없고 꼬리와 위치가 같은 경우
-    }
-
     class Tail {
-        int r;
-        int c;
+        int r; // row
+        int c; // col
 
         Tail (int r, int c) {
             this.r = r;
@@ -149,8 +145,8 @@ class Game {
     }
 
     class Direction {
-        int ttl;
-        String dir;
+        int ttl; // time to live
+        String dir; // 방향성
 
         Direction (int ttl, String dir) {
             this.ttl = ttl;
@@ -158,9 +154,3 @@ class Game {
         }
     }
 }
-
-// 0 0 0 0 0 0 -> 움직이니까 + 1
-
-// 7 7 7 7 0 0
-// 0 7 0 7
-// 0 7 7 7
